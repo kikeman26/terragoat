89a7a901e4f38e42eedc33955e7162f73c1dbc46
Hiram Rosales <57599263+kikeman26@users.noreply.github.com>
2023-09-12 13:37:59 -0600

    Create s3.tf

---
+++ terraform/simple_instance/s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "dockingbay" {
+  bucket_prefix = "docking-bay-storage-"
+
+  tags = {
+    Name                 = "Docking Bay"
+    Environment          = "Dev"
+  }
+}
