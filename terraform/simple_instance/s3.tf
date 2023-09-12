bab74ff918ba07336e3101d92c13399419950b26
Hiram Rosales <57599263+kikeman26@users.noreply.github.com>
2023-09-12 13:00:33 -0600

    Update s3.tf

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "dockingbay" {
+resource "aws_s3_bucket" "docking_bay" {
   bucket_prefix = "docking-bay-storage-"
 
   tags = {
